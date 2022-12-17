eae6bc19419a6f400a83403c296deec3f2c404c2
zhenya <zhenyaprokopev0@gmail.com>
2022-12-17 13:20:07 +0400

    signature authorization and db

---
+++ 1C.U/AuthorizationForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1C.U
+{
+    public partial class AuthorizationForm : Form
+    {
+        private string Nick { set; get; }
+        private string Password { set; get; }
+
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckUserData(object sender, EventArgs e)
+        {
+           
+        }
+
+        private bool IsUserExist()
+        {
+            
+        }
+    }
+}
---
+++ 1C.U/DataDase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1C.U
+{
+    class DataBase
+    {
+        
+
+        public static List<User> LoadUsersData()
+        {
+            
+        }
+
+        public static void LoadItemsData()
+        {
+            
+        }
+
+        public static void DeleteRow(string id)
+        {
+           
+        }
+
+        public static void AddRow(string values)
+        {
+           
+        }
+
+        public static void AddUser(string values)
+        {
+            
+        }
+    }
+}
