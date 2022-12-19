329e4a849eaa9a08564cc147be276041b00b50ab
Bazilevskijs <bazilevskijsemen@gmail.com>
2022-12-19 14:22:04 +0400

    added class user

---
+++ 1C.U/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1C.U
+{
+    class User
+    {
+        public int Id { set; get; }
+        public string NickName { set; get; }
+        public string Password { set; get; }
+    }
+
+    class Admin : User
+    {
+
+    }
+}
